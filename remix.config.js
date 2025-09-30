b55cc3851f9de4af9a44fd527e2d3df549305776
hoclac1225 <hoclac1225@gmail.com>
2025-09-30 19:22:45 +0700

    Add remix.config.js to repo

---
+++ remix.config.js
@@ -0,0 +1,31 @@
+// Nếu HOST được set và SHOPIFY_APP_URL chưa set hoặc trùng với HOST, dùng HOST làm URL
+if (
+    process.env.HOST &&
+    (!process.env.SHOPIFY_APP_URL || process.env.SHOPIFY_APP_URL === process.env.HOST)
+  ) {
+    process.env.SHOPIFY_APP_URL = process.env.HOST;
+    delete process.env.HOST;
+  }
+  
+  /** @type {import('@remix-run/dev').AppConfig} */
+  module.exports = {
+    appDirectory: "app",
+    assetsBuildDirectory: "public/build",
+    serverBuildPath: "build/server/index.cjs",
+    publicPath: "/build/",
+    serverModuleFormat: "cjs",
+    ignoredRouteFiles: ["**/.*"],
+  
+    serverDependenciesToBundle: [
+      /^@shopify\/polaris(\/.*)?$/,
+      /^@shopify\/shopify-app-remix(\/.*)?$/
+    ],
+  
+    dev: {
+      port: Number(process.env.PORT) || 60600,
+      strictPort: false, // false để tự tăng port nếu bận
+      host: '127.0.0.1', // ép IPv4
+      watchPaths: ["app", "remix.config.cjs"]
+    }
+  };
+  
