44ae502af6269bdb1b4d05da5c6a46891f30e3ad
hoclac1225-spec <hoclac1225@gmail.com>
2025-09-30 18:07:05 +0700

    Update and rename en.json to en.js

---
+++ app/locales/en.js
@@ -0,0 +1,11 @@
+export default {
+  "Polaris": {
+    "Common": {
+      "pagination": { "next": "Next", "previous": "Previous" },
+      "button": { "save": "Save", "cancel": "Cancel", "delete": "Delete" },
+      "loading": "Loading...",
+      "empty": "No data available"
+    },
+    // ... rest
+  }
+}
