92d47c3c8717848d8c9ea413ef4ef8671672dd90
hoclac1225-spec <hoclac1225@gmail.com>
2025-10-01 17:33:05 +0700

    Update remix.config.cjs

--- remix.config.cjs
+++ remix.config.cjs
@@ -1,31 +1,31 @@
+// remix.config.cjs
 // Nếu HOST được set và SHOPIFY_APP_URL chưa set hoặc trùng với HOST, dùng HOST làm URL
 if (
-    process.env.HOST &&
-    (!process.env.SHOPIFY_APP_URL || process.env.SHOPIFY_APP_URL === process.env.HOST)
-  ) {
-    process.env.SHOPIFY_APP_URL = process.env.HOST;
-    delete process.env.HOST;
+  process.env.HOST &&
+  (!process.env.SHOPIFY_APP_URL || process.env.SHOPIFY_APP_URL === process.env.HOST)
+) {
+  process.env.SHOPIFY_APP_URL = process.env.HOST;
+  delete process.env.HOST;
+}
+
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  serverBuildPath: "build/server/index.cjs",
+  publicPath: "/build/",
+  serverModuleFormat: "cjs",
+  ignoredRouteFiles: ["**/.*"],
+
+  serverDependenciesToBundle: [
+    /^@shopify\/polaris(\/.*)?$/,
+    /^@shopify\/shopify-app-remix(\/.*)?$/
+  ],
+
+  dev: {
+    port: Number(process.env.PORT) || 60600,
+    strictPort: false,
+    host: "127.0.0.1",
+    watchPaths: ["app", "remix.config.cjs"]
   }
-  
-  /** @type {import('@remix-run/dev').AppConfig} */
-  module.exports = {
-    appDirectory: "app",
-    assetsBuildDirectory: "public/build",
-    serverBuildPath: "build/server/index.cjs",
-    publicPath: "/build/",
-    serverModuleFormat: "cjs",
-    ignoredRouteFiles: ["**/.*"],
-  
-    serverDependenciesToBundle: [
-      /^@shopify\/polaris(\/.*)?$/,
-      /^@shopify\/shopify-app-remix(\/.*)?$/
-    ],
-  
-    dev: {
-      port: Number(process.env.PORT) || 60600,
-      strictPort: false, // false để tự tăng port nếu bận
-      host: '127.0.0.1', // ép IPv4
-      watchPaths: ["app", "remix.config.cjs"]
-    }
-  };
-  
+};
