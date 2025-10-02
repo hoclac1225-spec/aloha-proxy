2e43e5eae41f46f5f8d35aceec4bd19ce4ab7a89
hoclac1225-spec <hoclac1225@gmail.com>
2025-10-02 21:15:23 +0700

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,26 @@
 /** @type {import('@types/eslint').Linter.BaseConfig} */
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+    jest: true,
+  },
   extends: [
     "@remix-run/eslint-config",
     "@remix-run/eslint-config/node",
     "@remix-run/eslint-config/jest-testing-library",
-    "prettier",
+    "prettier"
   ],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
   globals: {
-    shopify: "readonly"
+    shopify: "readonly",
+  },
+  rules: {
+    // Thêm các rule tuỳ chỉnh nếu muốn
   },
 };
